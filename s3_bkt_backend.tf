b40f07280b5f626d9ac36ea3bd5eaa10236c2224
sudhakar-pujair <sudhakar.pujair1990@gmail.com>
2025-11-23 19:35:54 +0530

    third commit

---
+++ s3_bkt_backend.tf
@@ -0,0 +1,14 @@
+resource "random_integer" "s3suffix" {
+  min = 1000
+  max = 3000
+}
+resource "aws_s3_bucket" "bucket_backend" {
+  region        = "us-east-1"
+  bucket        = "terraform-${random_integer.s3suffix.result}"
+  force_destroy = true
+  tags = {
+    Name        = "terraform-${random_integer.s3suffix.result}"
+    Environment = "test"
+  }
+
+}
