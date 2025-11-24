3f1998c41e93c9eb12b7bb381100ed3a364c6bc1
sudhakar-pujair <sudhakar.pujair1990@gmail.com>
2025-11-24 15:07:40 +0530

    cleanup the codes

--- dev/subnets.tf
+++ dev/subnets.tf
@@ -3,7 +3,7 @@ resource "aws_subnet" "public_subnet" {
     cidr_block = var.public_subnet_cidr
 
     tags = {
-      Name = lower(var.public_sub_name)
+      Name = "${lower(var.public_sub_name)}-public"
       Environment = lower(var.environment)
     }
 }
@@ -13,7 +13,7 @@ resource "aws_subnet" "private_subnet" {
     cidr_block = var.private_subnet_cidr
 
     tags = {
-      Name = lower(var.private_sub_name)
+      Name = "${lower(var.private_sub_name)}-private"
       Environment = lower(var.environment)
     }
 }
