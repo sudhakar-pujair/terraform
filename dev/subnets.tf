829eea39bd1cec4b22ee9ea4798d3a5182b1eb61
sudhakar-pujair <sudhakar.pujair1990@gmail.com>
2025-11-24 15:29:09 +0530

    cleanup the codes

--- dev/subnets.tf
+++ dev/subnets.tf
@@ -1,19 +1,19 @@
-resource "aws_subnet" "public_subnet" {
+resource "aws_subnet" "public_subnet_1" {
     vpc_id     = aws_vpc.vpc_dev.id
-    cidr_block = var.public_subnet_cidr
+    cidr_block = var.public_sub_cidr_1
 
     tags = {
-      Name = "${lower(var.vpc_name)}-public"
+      Name = "${lower(var.vpc_name)}-public-1"
       Environment = lower(var.environment)
     }
 }
 
-resource "aws_subnet" "private_subnet" {
+resource "aws_subnet" "public_subnet_2" {
     vpc_id     = aws_vpc.vpc_dev.id
-    cidr_block = var.private_subnet_cidr
+    cidr_block = var.public_sub_cidr_1
 
     tags = {
-      Name = "${lower(var.vpc_name)}-private"
+      Name = "${lower(var.vpc_name)}-public-2"
       Environment = lower(var.environment)
     }
 }
