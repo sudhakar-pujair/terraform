17044c96d515ce9f49bac96bbededb1b13e3cdbe
sudhakar-pujair <sudhakar.pujair1990@gmail.com>
2025-11-24 15:30:49 +0530

    cleanup the codes

--- dev/route.tf
+++ dev/route.tf
@@ -12,22 +12,12 @@ resource "aws_route_table" "public_rt" {
   }
 }
 
-resource "aws_route_table_association" "public_subnet" {
-  subnet_id      = aws_subnet.public_subnet.id
+resource "aws_route_table_association" "public_subnet_1" {
+  subnet_id      = aws_subnet.public_subnet_1.id
   route_table_id = aws_route_table.public_rt.id
 }
 
-
-resource "aws_route_table" "private_rt" {
-  vpc_id = aws_vpc.vpc_dev.id
-
-  tags = {
-    Name = "${lower(var.vpc_name)}-private-rt"
-    Environment = lower(var.environment)
-  }
-}
-
-resource "aws_route_table_association" "private_rt" {
-  subnet_id      = aws_subnet.private_subnet.id
-  route_table_id = aws_route_table.private_rt.id
+resource "aws_route_table_association" "public_subnet_1" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_rt.id
 }
