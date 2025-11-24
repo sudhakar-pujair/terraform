09ddf3e98fd7ffa3455cc762880fa37cc15c7d75
sudhakar-pujair <sudhakar.pujair1990@gmail.com>
2025-11-24 15:31:19 +0530

    cleanup the codes

--- dev/route.tf
+++ dev/route.tf
@@ -17,7 +17,7 @@ resource "aws_route_table_association" "public_subnet_1" {
   route_table_id = aws_route_table.public_rt.id
 }
 
-resource "aws_route_table_association" "public_subnet_1" {
-  subnet_id      = aws_subnet.public_subnet_1.id
+resource "aws_route_table_association" "public_subnet_2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
   route_table_id = aws_route_table.public_rt.id
 }
