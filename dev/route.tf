d5e116ace7c12ed9a3de1f63868a33f1655e7c7f
sudhakar-pujair <sudhakar.pujair1990@gmail.com>
2025-11-24 15:21:17 +0530

    cleanup the codes

--- dev/route.tf
+++ dev/route.tf
@@ -20,12 +20,7 @@ resource "aws_route_table_association" "public_subnet" {
 
 resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.vpc_dev.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
+  
   tags = {
     Name = "${lower(var.vpc_name)-private-rt}"
     Environment = lower(var.environment)
