8cbbb7cd61883787edb0d5bd9b47c5df1a8d8ec4
sudhakar-pujair <sudhakar.pujair1990@gmail.com>
2025-11-25 13:02:36 +0530

    Re-written all codes

--- dev/providers.tf
+++ dev/providers.tf
@@ -5,13 +5,13 @@ terraform {
       version = "6.22.1"
     }
   }
-  # backend "s3" {
-  #   bucket       = "testterras"
-  #   key          = "test/terraform.tfstate"
-  #   region       = "us-east-1"
-  #   use_lockfile = true # This line enables S3 native locking
-  #   encrypt = true
-  # }
+  backend "s3" {
+    bucket       = "testterras"
+    key          = "test/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true # This line enables S3 native locking
+    encrypt = true
+  }
 }
 
 provider "aws" {
