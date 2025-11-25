ab132c574574705925384b939fca1cb6defdb6e6
sudhakar-pujair <sudhakar.pujair1990@gmail.com>
2025-11-25 10:58:50 +0530

    Re-written all codes

--- dev/providers.tf
+++ dev/providers.tf
@@ -5,14 +5,13 @@ terraform {
       version = "6.22.1"
     }
   }
-  # backend "s3" {
-  #   bucket       = "devsecopsb49tfstate"
-  #   key          = "class/terraform.tfstate"
-  #   region       = "us-east-1"
-  #   use_lockfile = true # This line enables S3 native locking
-  #   #dynamodb_table = "devsecopsb49_terraform_state_lock"
-  #   encrypt = true
-  # }
+  backend "s3" {
+    bucket       = "devterras"
+    key          = "dev/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true # This line enables S3 native locking
+    encrypt = true
+  }
 }
 
 provider "aws" {
