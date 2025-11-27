aea97aef375b6fba3c24a21bb724d684c21d991b
sudhakar-pujair <sudhakar.pujair1990@gmail.com>
2025-11-27 14:08:40 +0530

    created security group and ec2 instance with nginx

--- variables.tf
+++ variables.tf
@@ -1,26 +1,52 @@
 # VPC Variables
-variable "region" { type = string }
-variable "vpc_name" { type = string }
-variable "vpc_cidr" { type = string }
+variable "region" { 
+  type = string 
+}
+variable "vpc_name" { 
+  type = string 
+}
+variable "vpc_cidr" { 
+  type = string 
+}
 
 
 # Subnet Variables
-variable "public_subnet_cidrs" { type = list(string) }
-variable "private_subnet_cidrs" { type = list(string) }
-variable "azs" { type = list(string) }
+variable "public_subnet_cidrs" { 
+  type = list(string) 
+}
+variable "private_subnet_cidrs" { 
+  type = list(string) 
+}
+variable "azs" { 
+  type = list(string) 
+}
 
 # Aws instance variables
-variable "ami" { type = string }
-variable "instance_type" { type = string }
-variable "key_name" { type = string }
-variable "associate_public_ip_address" { type = bool }
+variable "ami" { 
+  type = string 
+}
+variable "instance_type" { 
+  type = string 
+}
+variable "key_name" { 
+  type = string 
+}
+variable "associate_public_ip_address" { 
+  type = bool 
+}
 
 # Security Group Variable Values
-variable "all_traffic" { type = string }
+variable "all_traffic" { 
+  type = string 
+}
 
 # S3 Variables
-variable "bucket_name" { type = string }
+variable "bucket_name" { 
+  type = string 
+}
 
 # Common Variables
-variable "env" { type = string }
+variable "env" { 
+  type = string 
+}
 
