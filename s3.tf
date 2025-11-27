7be95e78177fcf7d34f76f9a47041528b06d7ab4
sudhakar-pujair <sudhakar.pujair1990@gmail.com>
2025-11-27 07:49:07 +0530

    using terraform conditions

--- s3.tf
+++ s3.tf
@@ -17,20 +17,16 @@ resource "aws_s3_bucket" "s3_bucket" {
 
   lifecycle {
     precondition {
-      condition     = local.bucket_name == lower(local.bucket_name)
-      error_message = "Bucket prefix must be lowercase."
+      condition     = alltrue([length(local.bucket_name) > 14, length(local.bucket_name) < 21])
+      error_message = "The Length of Bucket Name Must Be Between 15 to 20."
     }
-    # precondition {
-    #   condition     = alltrue([length(local.bucket_name) > 14, length(local.bucket_name) < 21])
-    #   error_message = "The Length of Bucket Name Must Be Between 15 to 20."
-    # }
-    # precondition {
-    #   condition     = contains(["dev", "DEV", "prod", "PROD"], var.env)
-    #   error_message = "The Bucket Is Not Allowed To Be Created In Environment ${var.env}"
-    # }
-    # precondition {
-    #   condition     = anytrue([var.env == "dev", var.env == "DEV", var.env == "prod", var.env == "PROD"])
-    #   error_message = "The Environment Must be Dev or Prod"
-    # }
+    precondition {
+      condition     = contains(["dev", "DEV", "prod", "PROD"], var.env)
+      error_message = "The Bucket Is Not Allowed To Be Created In Environment ${var.env}"
+    }
+    precondition {
+      condition     = anytrue([var.env == "dev", var.env == "DEV", var.env == "prod", var.env == "PROD"])
+      error_message = "The Environment Must be Dev or Prod"
+    }
   }
 }
