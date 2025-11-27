9514ea1605adfcd5dadecd5dd644a5b95d3e6960
sudhakar-pujair <sudhakar.pujair1990@gmail.com>
2025-11-27 08:03:03 +0530

    using terraform conditions

--- variables.tf
+++ variables.tf
@@ -41,7 +41,14 @@ variable "associate_public_ip_address" {
 }
 
 # S3 Variables
-variable "bucket_name" {}
+variable "bucket_name" {
+  type = string
+
+  # validation {
+  #   condition     = var.bucket_name == lower(var.bucket_name)
+  #   error_message = "bucket_name must be all lowercase."
+  # }
+}
 
 # Common Variables
 variable "env" {
