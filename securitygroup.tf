7226f61b96a0d53c489c0ab6b4ebc74b09f8f25b
sudhakar-pujair <sudhakar.pujair1990@gmail.com>
2025-11-27 13:31:44 +0530

    created security group and ec2 instance with nginx

--- securitygroup.tf
+++ securitygroup.tf
@@ -1,24 +1,37 @@
+# ---------------------------
+# Security Group Definition
+# ---------------------------
 resource "aws_security_group" "nginx_sg" {
   name        = "${var.vpc_name}-nginx-sg"
-  description = "Allow TLS inbound traffic and all outbound traffic"
+  description = "Allow inbound HTTP and all outbound traffic"
   vpc_id      = aws_vpc.dev_vpc.id
 
   tags = {
-    Name = "${var.vpc_name}-nginx-sg"
+    Name        = "${var.vpc_name}-nginx-sg"
     Environment = var.env
   }
 }
 
+# ---------------------------
+# Ingress Rule (HTTP 80)
+# ---------------------------
 resource "aws_vpc_security_group_ingress_rule" "nginx_sg_in" {
   security_group_id = aws_security_group.nginx_sg.id
-  cidr_ipv4         = var.cidr_ipv4
-  from_port         = 80
-  ip_protocol       = "tcp"
-  to_port           = 80
+
+  cidr_ipv4   = var.cidr_ipv4   # "0.0.0.0/0" from terraform.tfvars
+  from_port   = 80
+  to_port     = 80
+  ip_protocol = "tcp"
 }
 
+# ---------------------------
+# Egress Rule (Allow all)
+# ---------------------------
 resource "aws_vpc_security_group_egress_rule" "nginx_sg_out" {
   security_group_id = aws_security_group.nginx_sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 0
+  to_port     = 0
+  ip_protocol = "-1"  # all traffic
 }
