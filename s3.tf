63b18b81daff0c5371ce518126e689f798afdf69
sudhakar-pujair <sudhakar.pujair1990@gmail.com>
2025-11-27 08:05:42 +0530

    using terraform conditions

--- s3.tf
+++ s3.tf
@@ -15,21 +15,5 @@ resource "aws_s3_bucket" "s3_bucket" {
     Name        = local.bucket_name
     Environment = var.env
   }
-
-  lifecycle {
-    precondition {
-      condition     = local.bucket_name == lower(local.bucket_name)
-      error_message = "Bucket prefix must be lowercase."
-    }
-
-    precondition {
-      condition     = alltrue([length(local.bucket_name) >= 15, length(local.bucket_name) <= 20])
-      error_message = "The bucket name length must be between 15 and 20 characters."
-    }
-
-    precondition {
-      condition     = var.env == "dev" || var.env == "prod"
-      error_message = "The environment must be 'dev' or 'prod'."
-    }
-  }
+  
 }
