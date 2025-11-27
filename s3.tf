abca8b10ede88846369014295d34b2d112c170c0
sudhakar-pujair <sudhakar.pujair1990@gmail.com>
2025-11-27 07:51:53 +0530

    using terraform conditions

--- s3.tf
+++ s3.tf
@@ -4,29 +4,33 @@ resource "random_integer" "s3random" {
 }
 
 locals {
-  bucket_name = "${var.bucket_name}bucket${random_integer.s3random.result}"
+  bucket_name = "${lower(var.bucket_name)}bucket${random_integer.s3random.result}"
+  env_lower   = lower(var.env)
 }
+
 resource "aws_s3_bucket" "s3_bucket" {
   bucket        = local.bucket_name
   force_destroy = true
 
   tags = {
     Name        = local.bucket_name
-    Environment = var.env
+    Environment = local.env_lower
   }
 
   lifecycle {
     precondition {
-      condition     = alltrue([length(local.bucket_name) > 14, length(local.bucket_name) < 21])
-      error_message = "The Length of Bucket Name Must Be Between 15 to 20."
+      condition     = local.bucket_name == lower(local.bucket_name)
+      error_message = "Bucket prefix must be lowercase."
     }
+
     precondition {
-      condition     = contains(["dev", "DEV", "prod", "PROD"], var.env)
-      error_message = "The Bucket Is Not Allowed To Be Created In Environment ${var.env}"
+      condition     = alltrue([length(local.bucket_name) >= 15, length(local.bucket_name) <= 20])
+      error_message = "The bucket name length must be between 15 and 20 characters."
     }
+
     precondition {
-      condition     = anytrue([var.env == "dev", var.env == "DEV", var.env == "prod", var.env == "PROD"])
-      error_message = "The Environment Must be Dev or Prod"
+      condition     = local.env_lower == "dev" || local.env_lower == "prod"
+      error_message = "The environment must be 'dev' or 'prod'."
     }
   }
 }
