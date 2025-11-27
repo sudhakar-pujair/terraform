e02beb23b2c1f9b6f9cbcfff05a0b9764485feb7
sudhakar-pujair <sudhakar.pujair1990@gmail.com>
2025-11-27 13:21:21 +0530

    created security group and ec2 instance with nginx

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2_nginx" {
   ami = var.ami
   instance_type = var.instance_type
-  subnet_id = aws_subnet.public_subnets.id
+  subnet_id = aws_subnet.public_subnets[0].id
   vpc_security_group_ids = aws_security_group.nginx_sg.id
   key_name = var.key_name
   associate_public_ip_address = var.associate_public_ip_address
