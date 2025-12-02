a83c108c2aca20d2b64d6b698ac623d3d788ed1f
sudhakar-pujair <sudhakar.pujair1990@gmail.com>
2025-12-02 16:52:01 +0530

    created the tf files to launch instance

--- modules/compute/public_ec2.tf
+++ modules/compute/public_ec2.tf
@@ -1,13 +1,14 @@
 resource "aws_instance" "public-server" {
+  count                       = var.environment == "Devlopment" || var.environment == "dev" ? 1 : 1
   ami = lookup(var.amis, var.aws_region)
   instance_type = var.instance_type
   key_name = var.key_name
-  subnet_id = element(var.public_subnets)
+  subnet_id = element(var.public_subnets, count.index)
   vpc_security_group_ids = [var.sg_id]
   associate_public_ip_address = true
 
   tags = {
-    Name        = lower("${var.vpc_name}-Public-Server")
+    Name        = lower("${var.vpc_name}-Public-Server-${count.index + 1}")
     environment = lower("${var.environment}")
     Project = "${local.pname}"
   }
@@ -22,7 +23,7 @@ resource "aws_instance" "public-server" {
     sudo cp webhooktesting/index.html /var/www/html/index.nginx-debian.html
     sudo cp webhooktesting/style.css /var/www/html/style.css
     sudo cp webhooktesting/scorekeeper.js /var/www/html/scorekeeper.js
-  	#echo "<div><h1>${var.vpc_name}-Public-Server</h1></div>" >> /var/www/html/index.nginx-debian.html
-    sed -i '29i <center><div id="container"><h1>${var.vpc_name}-Public-Server</h1></div></center>' /var/www/html/index.nginx-debian.html
+  	#echo "<div><h1>${var.vpc_name}-Public-Server-${count.index + 1}</h1></div>" >> /var/www/html/index.nginx-debian.html
+    sed -i '29i <center><div id="container"><h1>${var.vpc_name}-Public-Server-${count.index + 1}</h1></div></center>' /var/www/html/index.nginx-debian.html
   EOF
 }
